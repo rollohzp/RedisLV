eb8bf03a5eb6b0c7e156db0f88b8f4145fec3713
ivanabc <ivanfanfan@126.com>
2015-07-27 18:18:04 +0800

    删除错误提交

--- src/release.h
+++ src/release.h
@@ -1,3 +1,3 @@
-#define REDIS_GIT_SHA1 "25586fde"
+#define REDIS_GIT_SHA1 "be7a288f"
 #define REDIS_GIT_DIRTY "13"
-#define REDIS_BUILD_ID "master-1437992012"
+#define REDIS_BUILD_ID "master-1437992148"
